a094781494b5f575e936fc9109650d5e4f1fae7c
WebLab04 <ivanlabunets209@gmail.com>
2024-01-22 21:57:30 +0100

    n

---
+++ script.js
@@ -0,0 +1,7 @@
+const hours = +prompt('Введіть будь ласка кількість годин: ');
+
+const seconds = hours * 3600;
+
+const resultMessage = `У ${hours} годинах - ${seconds} секунд`;
+
+alert(resultMessage);
